065050a66c19b33f45c711b683a1d0a95bea52cc
Leo <nicolas.leo.hu@gmail.com>
2021-08-16 13:21:29 +0800

    Update constant.hpp

--- constant.hpp
+++ constant.hpp
@@ -1,1 +1,11 @@
+#ifndef _CONSTANT_HPP
+#define _CONSTANT_HPP
+
+const char L_BRACE = '(';
+const char R_BRACE = ')';
+const char STR_START = '[';
+const char STR_END   = ']';
+const char STR_SPACE = ' ';
+
+#endif
 
